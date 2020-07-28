6c0856ad109464a26e26abbbf1dfeb3bc0a18f02
ndsatu <mdsatu.pro@gmail.com>
2020-07-29 00:23:10 +0600

    ...

--- resources/views/back/auth/master.blade.php
+++ resources/views/back/auth/master.blade.php
@@ -11,11 +11,11 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="{{asset('back/bower_components/bootstrap/dist/css/bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{asset('back/bootstrap/css/bootstrap.min.css')}}">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{asset('back/bower_components/font-awesome/css/font-awesome.min.css')}}">
+  <link rel="stylesheet" href="{{asset('back/font-awesome/css/font-awesome.min.css')}}">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="{{asset('back/bower_components/Ionicons/css/ionicons.min.css')}}">
+  <link rel="stylesheet" href="{{asset('back/Ionicons/css/ionicons.min.css')}}">
   <!-- Theme style -->
   <link rel="stylesheet" href="{{asset('back/dist/css/AdminLTE.min.css')}}">
 
@@ -45,8 +45,8 @@
 <!-- /.login-box -->
 
 <!-- jQuery 3 -->
-<script src="{{asset('back/bower_components/jquery/dist/jquery.min.js')}}"></script>
+<script src="{{asset('back/js/jquery.min.js')}}"></script>
 <!-- Bootstrap 3.3.7 -->
-<script src="{{asset('back/bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('back/bootstrap/js/bootstrap.min.js')}}"></script>
 </body>
 </html>
